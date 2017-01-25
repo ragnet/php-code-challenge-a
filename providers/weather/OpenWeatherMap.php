2d8fe40054240ee5b8999d63613a9fc73e62a421
Rogério Gonçalves <ragnet@gmail.com>
2017-01-25 15:41:54 -0500

    Added OpenWeatherMap provider implementation

--- providers/weather/OpenWeatherMap.php
+++ providers/weather/OpenWeatherMap.php
@@ -2,7 +2,31 @@
 
 class OpenWeatherMap extends Provider {
 
+    function OpenWeatherMap(){
+
+        $this->setTag( "openweathermap" );
+        $this->setKey( "6103b0f582e78c7382bc6b0cdc06deb8" );
+        $this->setUrl( "http://api.openweathermap.org/data/2.5/weather?q={city}&appid=" . $this->getKey() );
+
+    }
+
     function getFormattedResponse( $data ){
+
+        $response = [
+            "ip" => "----",
+            "city" => "----",
+            "temperature" => [
+                "current" => $data->main->temp,
+                "low" => $data->main->temp_min,
+                "high" => $data->main->temp_max
+            ],
+            "wind" => [
+                "speed" => $data->wind->speed,
+                "direction" => $data->wind->deg // @fixme Is this right wind direction property?
+            ]
+        ];
+
+        return $response;
 
     }
 
